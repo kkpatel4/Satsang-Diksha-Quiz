ffc7d449f76ae5ac1899838acd6329fadfe2901f
Kush Patel <kkpatel4@live.unc.edu>
2021-06-14 20:55:13 -0400

    Add answer choices and feedback

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ export const bank = [
 		questionText: 'Swaminarayan Bhagwan etle ke sākshāt Akshar-Purushottam Maharaj sarvane param ____, ____ ane ____ arpe.',
 		shloka: 1,
 		answerOptions: [
+			{ answerText: 'shanti, anand, sukh', isCorrect: true },
 			{ answerText: 'shanti, anand, dukh', isCorrect: false },
 			{ answerText: 'anand, shanti, sukh', isCorrect: false },
-			{ answerText: 'shanti, anand, sukh', isCorrect: true },
 			{ answerText: 'bhakti, dharma, maya', isCorrect: false },
 		]
 	},
@@ -15,8 +15,8 @@ export const bank = [
 		questionText: 'Ā deh _____ sādhan chhe, keval _____ sādhan nathi.',
 		shloka: 2,
 		answerOptions: [
-			{ answerText: 'bhognu, muktinu', isCorrect: false },
 			{ answerText: 'muktinu, bhognu', isCorrect: true },
+			{ answerText: 'bhognu, muktinu', isCorrect: false },
 			{ answerText: 'mokshanu, bhognu', isCorrect: false },
 			{ answerText: 'muktinu, dukhnu', isCorrect: false },
 		]
@@ -35,260 +35,260 @@ export const bank = [
 		questionText: '_____ vyavahār to dehnā _____ māte chhe.',
 		shloka: 3,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'Laukik, nirvah', isCorrect: true },
+			{ answerText: 'Alaukik, nirvah', isCorrect: false },
+			{ answerText: 'Laukik, dukh', isCorrect: false },
+			{ answerText: 'Laukik, sukh', isCorrect: false },
 		]
 	},
 	{
 		questionText: 'Te ā manushya janmanu param _____ nathi.',
 		shloka: 3,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'lakshya', isCorrect: true },
+			{ answerText: 'sāchan', isCorrect: false },
+			{ answerText: 'dhyey', isCorrect: false },
+			{ answerText: 'ānand', isCorrect: false },
 		]
 	},
 	{
 		questionText: 'Sarva _____ tālvā, _____ pāmvā ane Bhagwanni _____ karvā ā deh malyo chhe.',
 		shloka: 4,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'doshone, brahma-sthitine, bhakti', isCorrect: true },
+			{ answerText: 'swabhāvone, brahma-sthitine, dharma', isCorrect: false },
+			{ answerText: 'doshone, brahma-sthitine, dharma', isCorrect: false },
+			{ answerText: 'swabhāvone, moksha, dharma', isCorrect: false },
 		]
 	},
 	{
 		questionText: 'Ā badhu satsang karvāthi avashya _____ thāy chhe.',
 		shloka: 5,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'prapta', isCorrect: true },
+			{ answerText: 'sukh', isCorrect: false },
+			{ answerText: 'dukh', isCorrect: false },
+			{ answerText: 'mukti', isCorrect: false },
 		]
 	},
 	{
 		questionText: 'Āthi _____ sadāy satsang karvo.',
 		shloka: 5,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'mumukshuoe', isCorrect: true },
+			{ answerText: 'satsangie', isCorrect: false },
+			{ answerText: 'bhaktoe', isCorrect: false },
+			{ answerText: 'sadhuoe', isCorrect: false },
 		]
 	},
 	{
 		questionText: 'Tethi Parabrahma Swaminarayane ā _____ sākshāt avtarine ā divya _____ sthāpnā kari.',
 		shloka: 6,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'lokmā, satsangni', isCorrect: true },
+			{ answerText: 'deshmā, satsangni', isCorrect: false },
+			{ answerText: 'deshmā, mokshani', isCorrect: false },
+			{ answerText: 'lokmā, mokshani', isCorrect: false },
 		]
 	},
 	{
 		questionText: 'Ā satsangnu _____ mumukshuone thāy evā shubh āshaythi ‘Satsang Diksha’ e nāmnu shāstra _____ āve chhe',
 		shloka: 7,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'jnān, rachvāmā', isCorrect: true },
+			{ answerText: 'gnān, rachvāmā', isCorrect: false },
+			{ answerText: 'jnān, vanchvāma', isCorrect: false },
+			{ answerText: 'gnān, lakhvāmā', isCorrect: false },
 		]
 	},
 	{
 		questionText: 'Satya evā _____ sang karvo, satya evā _____ sang karvo, satya evā ____ sang karvo ane sach-chhāstrano sang karvo e satsangnu sāchu lakshan jānvu',
 		shloka: 8,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'ātmāno, Paramatmano, guruno', isCorrect: true },
+			{ answerText: 'guruno, Paramatmano, ātmāno', isCorrect: false },
+			{ answerText: 'ātmāno, guruno, Paramatmano', isCorrect: false },
+			{ answerText: 'guruno, ātmāno, Paramatmano', isCorrect: false },
 		]
 	},
 	{
 		questionText: 'Āvo divya satsang karnārā _____ sukhi thāy chhe.',
 		shloka: 9,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'manushya', isCorrect: true },
+			{ answerText: 'sadhu', isCorrect: false },
+			{ answerText: 'jiva', isCorrect: false },
+			{ answerText: 'Bhagwan', isCorrect: false },
 		],
 	},
 	{
 		questionText: 'Dikshā etle dradh ____, shraddhāe sahit evo achal ____, samyak samarpan, priti-purvak _____, vrat ane dradh āshro.',
 		shloka: 10,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'sankalp, nishchay, nishthā', isCorrect: true },
+			{ answerText: 'niyam, nishchay, paksha', isCorrect: false },
+			{ answerText: 'sevabhav, nishchay, paksha', isCorrect: false },
+			{ answerText: 'sankalp, nishthā, niyam', isCorrect: false },
 		],
 	},
 	{
-		questionText: 'Ā shāstramā Parabrahma Sahajanand Paramatmae darshāvel āgnā tathā _____ paddhatine _____ rite janāvi chhe.',
+		questionText: 'Ā shāstramā Parabrahma Sahajanand Paramatmae darshāvel _____ tathā _____ paddhatine spasht rite janāvi chhe.',
 		shloka: 11,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'āgnā, upāsanāni', isCorrect: true },
+			{ answerText: 'upāsanāni, āgnā', isCorrect: false },
+			{ answerText: 'niyam, āgnā', isCorrect: false },
+			{ answerText: 'niyam, upāsanāni', isCorrect: false },
 		],
 	},
 	{
 		questionText: 'Purusho tathā strio sarve _____ adhikāri chhe, sarve _____ adhikāri chhe ane sarve _____ adhikāri chhe.',
 		shloka: 12,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'satsangnā, sukhnā, brahmavidyānā', isCorrect: true },
+			{ answerText: 'sukhnā, satsangnā, brahmavidyānā', isCorrect: false },
+			{ answerText: 'brahmavidyānā, sukhnā, satsangnā', isCorrect: false },
+			{ answerText: 'satsangnā, brahmavidyānā, sukhnā', isCorrect: false },
 		],
 	},
 	{
-		questionText: 'Satsangmā _____-bhedthi _____-ādhikpanu na ja samajvu.',
+		questionText: 'Satsangmā ling-bhedthi _____-_______ na ja samajvu.',
 		shloka: 13,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'nyun-ādhikpanu', isCorrect: true },
+			{ answerText: 'nathi-ādhikpanu', isCorrect: false },
+			{ answerText: 'nyun-avgunpanu', isCorrect: false },
+			{ answerText: 'nyun-ādhikta', isCorrect: false },
 		],
 	},
 	{
 		questionText: 'Badhā pot-potāni _____ rahi bhakti vade _____ pāmi shake chhe.',
 		shloka: 13,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'maryādāmā, muktine', isCorrect: true },
+			{ answerText: 'gharmā, muktine', isCorrect: false },
+			{ answerText: 'maryādāmā, mokshane', isCorrect: false },
+			{ answerText: 'gharmā, mokshane', isCorrect: false },
 		],
 	},
 	{
 		questionText: 'Sarva _____ sarva strio tathā sarva purusho sadāy satsang, _____ ane _____ adhikāri chhe.',
 		shloka: 14,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'varnanā, brahmavidyā, mokshanā', isCorrect: true },
+			{ answerText: 'manushya, brahmavidyā, muktinā', isCorrect: false },
+			{ answerText: 'manushya, brahmavidyā, sukhnā', isCorrect: false },
+			{ answerText: 'manushya, jnān, muktinā', isCorrect: false },
 		],
 	},
 	{
 		questionText: 'Varnanā ādhāre kyārey nyun-_____ na karvo.',
 		shloka: 15,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'ādhikbhāv', isCorrect: true },
+			{ answerText: 'manushyabhāv', isCorrect: false },
+			{ answerText: 'gunbhāv', isCorrect: false },
+			{ answerText: 'sevabhāv', isCorrect: false },
 		],
 	},
 	{
 		questionText: 'Sarva janoe potānā _____ mān tyajine paraspar _____ karvi.',
 		shloka: 15,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'varnanu, sevā', isCorrect: true },
+			{ answerText: 'lingnu, bhakti', isCorrect: false },
+			{ answerText: 'varnanu, bhakti', isCorrect: false },
+			{ answerText: 'mannu, dharma', isCorrect: false },
 		],
 	},
 	{
 		questionText: 'Jātie karine koi _____ nathi ane koi _____ pan nathi.',
 		shloka: 16,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'mahān, nyun', isCorrect: true },
+			{ answerText: 'saru, nyun', isCorrect: false },
+			{ answerText: 'sukh, mahān', isCorrect: false },
+			{ answerText: 'nyun, mahān', isCorrect: false },
 		],
 	},
 	{
 		questionText: 'Tethi nātjātne laine _____ na karvo ne _____ satsang karvo.',
 		shloka: 16,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'klesh, sukhe', isCorrect: true },
+			{ answerText: 'klesh, dukhe', isCorrect: false },
+			{ answerText: 'abhāv, sukhe', isCorrect: false },
+			{ answerText: 'abhāv, dukhe', isCorrect: false },
 		],
 	},
 	{
 		questionText: '_____ tathā _____ sarve mokshanā adhikāri chhe.',
 		shloka: 17,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'Gruhastha, tyāgi', isCorrect: true },
+			{ answerText: 'Tyāgi, gruhastha', isCorrect: false },
+			{ answerText: 'Gruhastha, sadhu', isCorrect: false },
+			{ answerText: 'Sadhu, gruhastha', isCorrect: false },
 		],
 	},
 	{
 		questionText: 'Temā nyun-ādhikbhāvnathi, kāran ke gruhastha ke tyāgi _____ Bhagwannā _____ chhe.',
 		shloka: 17,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'badhā, bhakto', isCorrect: true },
+			{ answerText: 'sarve, bhakto', isCorrect: false },
+			{ answerText: 'badhā, haribhakto', isCorrect: false },
+			{ answerText: 'sarve, haribhakto', isCorrect: false },
 		],
 	},
 	{
 		questionText: 'Swaminarayan Bhagwanne vishe _____, dradh ane _____ bhakti māte āshraydikshā _____ grahan kari satsang prāpta karvo.',
 		shloka: 18,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'ananya, param, mantra', isCorrect: true },
+			{ answerText: 'param, ananya, shloka', isCorrect: false },
+			{ answerText: 'ananya, param, shloka', isCorrect: false },
+			{ answerText: 'param, dharma, mantra', isCorrect: false },
 		],
 	},
 	{
 		questionText: 'Āshray-dikshā mantra ā pramāne chhe: Dhanyo’smi purna-kāmo’smi _____ nirbhayah sukhi; _____-guru-yogena _____-āshrayat.',
 		shloka: 19,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'nishpāpo, Akshara, Swaminārāyan', isCorrect: true },
+			{ answerText: 'nishtha, Gunatit, Parabrahma', isCorrect: false },
+			{ answerText: 'nishpāpo, Akshar, Paramatma', isCorrect: false },
+			{ answerText: 'nishtha, Brahma, Swaminārāyan', isCorrect: false },
 		],
 	},
 	{
 		questionText: 'Mumukshu potānā _____ mukti māte Sahajanand Shri Hari tathā Aksharbrahma-swarup Gunatit guruno _____ karine _____ kare.',
 		shloka: 20,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'ātmāni, pritie, āshro', isCorrect: true },
+			{ answerText: 'paramatmani, pritie, āshro', isCorrect: false },
+			{ answerText: 'ātmāni, pritie, nishchay', isCorrect: false },
+			{ answerText: 'paramatmani, āshro, pritie', isCorrect: false },
 		],
 	},
 	{
 		questionText: 'Satsangno āshro kari sadāy _____ vishe kāshthni bevdi _____ dhāran karvi tathā satsangnā _____ dhāran karvā.',
 		shloka: 21,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'kanthne, mālā, niyamo', isCorrect: true },
+			{ answerText: 'kanthine, mālā, agna', isCorrect: false },
+			{ answerText: 'kanthne, pujā, niyamo', isCorrect: false },
+			{ answerText: 'kanthine, pujā, agna', isCorrect: false },
 		],
 	},
 	{
 		questionText: 'Ā sansārmā brahmaswarup guru vinā jivanmā _____ tattve karine _____ na thai shake.',
 		shloka: 22,
 		answerOptions: [
-			{ answerText: 'Apple', isCorrect: true },
-			{ answerText: 'Intel', isCorrect: false },
-			{ answerText: 'Amazon', isCorrect: false },
-			{ answerText: 'Microsoft', isCorrect: false },
+			{ answerText: 'brahmavidyāno, sākshātkār', isCorrect: true },
+			{ answerText: 'brahmavidyāno, nidhidhyas', isCorrect: false },
+			{ answerText: 'gnānno, sākshātkār', isCorrect: false },
+			{ answerText: 'gnānno, shravan', isCorrect: false },
 		],
 	}
 ];
@@ -298,10 +298,17 @@ export default function App() {
 	const [currentQuestion, setCurrentQuestion] = useState(0);
 	const [showScore, setShowScore] = useState(false);
 	const [score, setScore] = useState(0);
+	const [footer, setFooter] = useState('start');
+	const [correct, setCorrect] = useState('');
 
 	const handleAnswerOptionClick = (isCorrect) => {
 		if (isCorrect) {
 			setScore(score + 1);
+			setFooter('correct')
+			setCorrect('You were correct! Score: ' + score)
+		} else {
+			setFooter('incorrect')
+			setCorrect('You were incorrect! Score: ' + score)
 		}
 
 		const nextQuestion = currentQuestion + 1;
@@ -367,6 +374,7 @@ export default function App() {
 							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
 						))}
 					</div>
+					<footer className={footer}>{correct}</footer>
 				</>
 			)}
 		</div>
